5a69a9771b922874610df21d6202b61531245574
matheus <matheusemmanuel2d@gmail.com>
2024-06-24 15:13:44 -0300

    comentarios do projeto

--- conta-banco/src/contaTerminal.java
+++ conta-banco/src/contaTerminal.java
@@ -1,5 +1,11 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        //TODO: 
+     // TODO: Conhecer e importar a classe Scanner 
+
+      // Exibir as mensagems para o nosso usuario  
+
+      // Obter pela scanner os mvalores digitados no terminal
+      
+      // Exibir a mensagem conta criada
     }
 }
